908bad2d1d8c76ecaca80c9b98cbe7d023cf906a
Radu <daescuradu@yahoo.com>
2018-11-09 00:38:35 +0200

    Created new simpler methods

--- create_sql.py
+++ create_sql.py
@@ -1,51 +1,49 @@
 from __future__ import division
 from sql_lib import sql
-from collections import OrderedDict
+from get_csv import get_file_path
 from re import match
 import os, math
 
-def get_files():
-
-    cwd = os.getcwd() + '/data/'
-    csv_1 = []  #for big data (sp,ge,fr, etc. )
-    csv_2 = []  #for small data (ro, pol, arg, etc.)
-
-    for root, dirs, files in os.walk(cwd):
-        for f in files:
-
-            if f.endswith('csv') and '_' in f:
-                csv_1.append(root + '/' + f)
-
-            elif f.endswith('csv') and '_' not in f:
-                csv_2.append(root + '/' + f)
-
-    data = {'big': csv_1, 'small': csv_2}
-    return data
-
-def data_big(filename):
+def data_p(filename):
     
     content = file(filename, 'r').readlines()
     data = {}
-    header = content.pop(0).split(',')
+    content.pop(0)
     e = 2.71828
 
+    replace_dict = {'CS U. Craiova': 'U Craiova 1948 CS',
+                    'Daco-Getica Bucuresti': 'FC Juventus Bucuresti',
+                    'FC Viitorul': 'Viitorul Constanta'}
     values = ['gda', 'gpa', 'gdd', 'gpd', 'va', 'ea', 'ia', 'vd', 'ed', 'id']
+    ignore_teams = ['Neustadt', 'Chindia Targoviste', 'UTA Arad']
 
     for line in content:
         line = [l.strip() for l in line.split(',')]
+        
+        for rk, rv in replace_dict.items():
+            line = [j.replace(rk, rv) for j in line]
+
+        if line[2] in ignore_teams or line[3] in ignore_teams:
+            break
 
         if line[2] not in data.keys():
             data[line[2]] = {}
+            data[line[2]]['mdate'] = []
 
         if line[3] not in data.keys():
             data[line[3]] = {}
+            data[line[3]]['mdate'] = []
 
         for val in values:
             for index in [2,3]:
                 if val not in data[line[index]].keys():
                     data[line[index]][val] = 0
-        data[line[2]]['mdate' ] = int(''.join(line[1].split('/')[::-1]))
-        data[line[3]]['mdate' ] = int(''.join(line[1].split('/')[::-1]))
+
+        if line[4] == '':
+            line[4] = 0
+        if line[5] == '':
+            line[5] = 0
+
         data[line[2]]['gda'] += int(line[4])
         data[line[2]]['gpa'] += int(line[5])
         data[line[3]]['gdd'] += int(line[5])
@@ -54,14 +52,20 @@ def data_big(filename):
         if line[4] > line[5]:
             data[line[2]]['va'] += 1
             data[line[3]]['id'] += 1
+            data[line[2]]['mdate' ].append(''.join(line[1].split('/')[::-1]) + '+2')
+            data[line[3]]['mdate' ].append(''.join(line[1].split('/')[::-1]) + '-2')
 
         elif line[4] < line[5]:
             data[line[2]]['ia'] += 1
             data[line[3]]['vd'] += 1
+            data[line[2]]['mdate' ].append(''.join(line[1].split('/')[::-1]) + '-3')
+            data[line[3]]['mdate' ].append(''.join(line[1].split('/')[::-1]) + '+3')
 
-        elif line[4] == line[5]:
+        else:
             data[line[2]]['ea'] += 1
             data[line[3]]['ed'] += 1
+            data[line[2]]['mdate' ].append(''.join(line[1].split('/')[::-1]) + '+0')
+            data[line[3]]['mdate' ].append(''.join(line[1].split('/')[::-1]) + '+1')
 
     for key,val in data.items():
 
@@ -76,100 +80,6 @@ def data_big(filename):
         val['pct'] = (val['va'] + val['vd'])*3 + (val['ea'] + val['ed'])*1
         val['fa'] = float(format((val['va']-val['ia'])*100/val['mja'], '.1f'))
         val['fd'] = float(format((val['vd']-val['id'])*100/val['mjd'], '.1f'))
-
-        for g in range(5):
-            for t in ['gda', 'gpa', 'gdd', 'gpd']:
-                val['p%s%s' %(g,t)] = float(format(e**(-val['m%s' %t])*val['m%s' %t]**g*100/math.factorial(g), '.2f'))
-
-    export_list = ['mjt', 'pct', 'fa', 'fd', 'p[0-4]g[d,p][a,d]']
-    export_data = {}
-
-    for key,val in data.items():
-        export_data[key] = {}
-        for k in val:
-            for ex in export_list:
-                if match(ex, k):
-                    export_data[key][k] = data[key][k]
-
-    return export_data
-
-def data_small(filename, year = 2018):
-
-    content = file(filename, 'r').readlines()
-    data = {}
-    header = content.pop(0).split(',')
-    e = 2.71828
-    
-    replace_dict = {'CS U. Craiova': 'U Craiova 1948 CS',
-                    'Daco-Getica Bucuresti': 'FC Juventus Bucuresti'}
-    values = ['gda', 'gpa', 'gdd', 'gpd', 'va', 'ea', 'ia', 'vd', 'ed', 'id']
-
-    ignore_teams = ['Neustadt', 'Chindia Targoviste', 'UTA Arad']
-
-    for line in content:
-        line = [i.strip() for i in line.split(',')]
-        
-        for rk, rv in replace_dict.items():
-            line = [j.replace(rk, rv) for j in line]
-
-        if line[5] in ignore_teams or line[6] in ignore_teams:
-            pass
-        else:
-            if int(line[2][:4]) == year:
-
-                if line[5] not in data.keys():
-                    data[line[5]] = {}
-                    data[line[5]]['mdate'] = []
-
-                if line[6] not in data.keys():
-                    data[line[6]] = {}
-                    data[line[6]]['mdate'] = []
-                
-                for val in values:
-                    for index in [5,6]:
-                        if val not in data[line[index]].keys():
-                            data[line[index]][val] = 0
-
-                if line[7] == '':
-                    line[7] = 0
-                if line[8] == '':
-                    line[8] = 0
-                
-                data[line[5]]['gda'] += int(line[7])
-                data[line[5]]['gpa'] += int(line[8])
-                data[line[6]]['gdd'] += int(line[8])
-                data[line[6]]['gpd'] += int(line[7])
-
-                if line[7] > line[8]:
-                    data[line[5]]['va'] += 1
-                    data[line[6]]['id'] += 1
-                    data[line[5]]['mdate' ].append(''.join(line[3].split('/')[::-1]) + '+2')
-                    data[line[6]]['mdate' ].append(''.join(line[3].split('/')[::-1]) + '-2')
-
-                elif line[7] < line[8]:
-                    data[line[5]]['ia'] += 1
-                    data[line[6]]['vd'] += 1
-                    data[line[5]]['mdate' ].append(''.join(line[3].split('/')[::-1]) + '-3')
-                    data[line[6]]['mdate' ].append(''.join(line[3].split('/')[::-1]) + '+3')
-
-                elif line[7] == line[8]:
-                    data[line[5]]['ea'] += 1
-                    data[line[6]]['ed'] += 1
-                    data[line[5]]['mdate' ].append(''.join(line[3].split('/')[::-1]) + '+0')
-                    data[line[6]]['mdate' ].append(''.join(line[3].split('/')[::-1]) + '+1')
-
-    for key,val in data.items():
-        
-        val['mja'] = val['va'] + val['ea'] + val['ia']
-        val['mgda'] = float(format(val['gda']/val['mja'], '.2f'))
-        val['mgpa'] = float(format(val['gpa']/val['mja'], '.2f'))
-        val['mjd'] = val['vd'] + val['ed'] + val['id']
-        val['mgdd'] = float(format(val['gdd']/val['mjd'], '.2f'))
-        val['mgpd'] = float(format(val['gpd']/val['mjd'], '.2f'))
-        val['mjt'] = val['mja']+val['mjd']
-        val['pct'] = (val['va'] + val['vd'])*3 + (val['ea'] + val['ed'])*1
-        val['fa'] = float(format(val['va']-val['ia']*100/val['mja'], '.1f'))
-        val['fd'] = float(format(val['vd']-val['id']*100/val['mjd'], '.1f'))
         val['mdate'] = sorted(val['mdate'], reverse = True)[:10]
         val['istoric'] = ''.join([i[-2:] for i in val['mdate']])
         val['forta'] = 100
@@ -199,22 +109,11 @@ def data_small(filename, year = 2018):
 #for i,j in inter.items():
 #    print "%s: %r" %(i,j)
 
-for fl in get_files()['small']:
-    years = [2016, 2017, 2018]
-    liga = fl.split('/')[-1].rstrip('.csv')
-    for year in years:
-        dt = data_small(fl, year = year)
-        print 'Starting %s %s' %(liga, year)   
-        for k,v in sorted(dt.items()):
-            dic = {'name': k}
-            dic.update(v)
-            sql().add_value(db = 'data/all_%s.db' %year, tb = liga, **dic)
-
-for fl in get_files()['big']:
+for fl in get_file_path()['big']:
     year = fl.split('_')[-1].rstrip('.csv')
     liga = '_'.join(fl.split('/')[-1].split('_')[:2])
-    dt = data_big(fl)
     print 'Starting %s %s' %(liga, year)
+    dt = data_p(fl)
     if int(year) <= 2018:
         for k,v in sorted(dt.items()):
             dic = {'name': k}
--- get_csv.py
+++ get_csv.py
@@ -1,12 +1,26 @@
-#!/usr/bin/env python
-
 from links import all
 from wget import download
 from time import time
 import os
 import shutil
 
-#based on links, will download csv's
+def get_file_path():
+
+    cwd = os.getcwd() + '/data/'
+    csv_1 = []  #for big data (sp,ge,fr, etc. )
+    csv_2 = []  #for small data (ro, pol, arg, etc.)
+
+    for root, dirs, files in os.walk(cwd):
+        for f in files:
+
+            if f.endswith('csv') and '_' in f:
+                csv_1.append(root + '/' + f)
+
+            elif f.endswith('csv') and '_' not in f:
+                csv_2.append(root + '/' + f)
+
+    data = {'big': csv_1, 'small': csv_2}
+    return data
 
 def get_csv():
     
@@ -29,7 +43,7 @@ def get_csv():
 
         if isinstance(value, dict):
             for k,v in value.items():
-                if k == 2018:
+                if k <= 2018:
                     output1 = path+country+'/'+key+'_'+str(k)+'.csv'
                     file1 = download(v, out = output1)
                     if os.path.exists(output1):
@@ -42,6 +56,62 @@ def get_csv():
                 shutil.move(file2, output2)
 
     print '\nIt took %.2f seconds to download all files' %(time()-start)
+    return True
+
+def move_small(filename):
+
+    content = open(filename, 'r').readlines()
+    content.pop(0)
+    header = 'Div,Date,HomeTeam,AwayTeam,FTHG,FTAG\r\n'
+
+    """
+    f2012 = open(filename.replace('.csv', '_1_2012.csv'), 'w')
+    f2012.write(header)
+    f2013 = open(filename.replace('.csv', '_1_2013.csv'), 'w')
+    f2013.write(header)
+    f2014 = open(filename.replace('.csv', '_1_2014.csv'), 'w')
+    f2014.write(header)
+    f2015 = open(filename.replace('.csv', '_1_2015.csv'), 'w')
+    f2015.write(header)
+    """
+    f2016 = open(filename.replace('.csv', '_1_2016.csv'), 'w')
+    f2016.write(header)
+    f2017 = open(filename.replace('.csv', '_1_2017.csv'), 'w')
+    f2017.write(header)
+    f2018 = open(filename.replace('.csv', '_1_2018.csv'), 'w')
+    f2018.write(header)
+   
+    for line in content:
+        line = [l.strip() for l in line.split(',')]
+        
+        if line[2][:4] == '2018':
+            f2018.write('%s,%s,%s,%s,%s,%s\r\n' %(line[1], line[3], line[5], line[6], line[7], line[8]))
+        elif line[2][:4] == '2017':
+            f2017.write('%s,%s,%s,%s,%s,%s\r\n' %(line[1], line[3], line[5], line[6], line[7], line[8]))
+        elif line[2][:4] == '2016':
+            f2016.write('%s,%s,%s,%s,%s,%s\r\n' %(line[1], line[3], line[5], line[6], line[7], line[8]))
+        """
+        elif line[2][:4] == '2015':
+            f2015.write('%s,%s,%s,%s,%s,%s\r\n' %(line[1], line[3], line[5], line[6], line[7], line[8]))
+        elif line[2][:4] == '2014':
+            f2014.write('%s,%s,%s,%s,%s,%s\r\n' %(line[1], line[3], line[5], line[6], line[7], line[8]))
+        elif line[2][:4] == '2013':
+            f2013.write('%s,%s,%s,%s,%s,%s\r\n' %(line[1], line[3], line[5], line[6], line[7], line[8]))
+        elif line[2][:4] == '2012':
+            f2012.write('%s,%s,%s,%s,%s,%s\r\n' %(line[1], line[3], line[5], line[6], line[7], line[8]))
+    
+    f2012.close()
+    f2013.close()
+    f2014.close()
+    f2015.close()
+    """
+    f2016.close()
+    f2017.close()
+    f2018.close()
+
+    return True
 
 get_csv()
 
+for fl in get_file_path()['small']:
+    move_small(fl)
